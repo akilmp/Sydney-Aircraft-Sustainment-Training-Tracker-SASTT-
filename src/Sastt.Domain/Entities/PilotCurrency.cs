ab86e2928f4b480729a2abc6fc597eea91282e5f
Akil Mullassery Prashant <94084424+akilmp@users.noreply.github.com>
2025-08-20 20:54:48 +1000

    Add domain entities, enums, and events

---
+++ src/Sastt.Domain/Entities/PilotCurrency.cs
@@ -0,0 +1,8 @@
+namespace Sastt.Domain.Entities;
+
+public class PilotCurrency : Base
+{
+    public Guid PilotId { get; set; }
+    public string CurrencyType { get; set; } = string.Empty;
+    public DateTime ExpirationDate { get; set; }
+}
